4c93e6771b2b5b44f0676df0f8b98ba1c413f706
Czekobons <szymcon1997@gmail.com>
2023-02-02 19:30:09 +0100

    Zadanie 0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal dog = new Animal("Terrier", 5.0);
+        dog.feed();
+        for(int i=0;i<4;i++) {
+            dog.takeForWalk();
+        }
+        dog.feed();
+        dog.takeForWalk();
+    }
+
+}
